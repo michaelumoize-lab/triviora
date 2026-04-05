87110e0cf4da03509a8d891b26fac04010062737
Michael Umoize <michaelumoize@gmail.com>
2026-04-06 01:46:26 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Triviora | Personalized Life Management Platform",
+  description: "Triviora is a personalized life management platform designed to help you achieve balance across mind, body, and spirit. By tracking your daily activities—from spiritual practices and physical health to nutrition and productivity—Triviora provides intelligent insights and guidance to improve your overall well-being. Stay consistent, build meaningful habits, and live a more aligned and intentional life.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
